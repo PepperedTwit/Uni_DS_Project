e2cdd409219401f5b55d5135c566cc8592af0295
Jacob Saunders <ncnc.code@outlook.com>
2024-08-08 10:24:13 +1000

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::char::ToUppercase;
+
+use util::{io::*, public::{StrBox, IO}};
+
+fn main() {
+
+    println!("Hello, world!");
+
+    let data: Vec<Vec<StrBox>> = read_csv(&"land_council.csv").unwrap();
+
+    // let landc: Hash<StrBox> = Hash:with_capacity(10);
+
+    for x in data.iter() {
+
+        let name = x[2].to_ascii_lowercase();
+
+        if name.contains("land council") {
+
+            println!("{:?}", x);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
